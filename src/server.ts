02a54cf5a3dfdde5b3d472513f5d09095b91fc4d
Andy <andythomas.py@gmail.com>
2023-07-20 19:26:34 -0500

    Add cors functionality to server.ts and package.json

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,15 @@ import morgan from 'morgan';
 const app = express();
 const port = 3000;
 const logger = morgan('short');
+const cors = require('cors');
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
 app.use(logger);
+
+// Simple Usage (Enable All CORS Requests)
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
