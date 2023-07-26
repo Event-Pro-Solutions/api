4f838b09c3114d78f77e84e3921a5a79f9a67dbb
daltkb <daltkben@gmail.com>
2023-07-26 12:59:00 -0500

    Fix missing curly brace

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,15 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import { MongoClient, Db } from "mongodb";
-import { env } from "./config";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { MongoClient, Db } from 'mongodb';
+import { env } from './config';
 
 const app = express();
-const logger = morgan("short");
-
-async function connectToDatabase() {
-  const dbConnectionStr = env.DB_STRING;
-  const dbName = "eventPro";
-
-  try {
-    const client = await MongoClient.connect(dbConnectionStr);
-    console.log(`Connected to ${dbName} Database`);
-    return client.db(dbName) as Db;
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-  }
 const logger = morgan('short');
 
-// Use .env file in config folder
-require('dotenv').config({ path: './config/.env' });
-
 async function connectToDatabase() {
-    const dbConnectionStr = process.env.DB_STRING;
+    const dbConnectionStr = env.DB_STRING;
     const dbName = 'eventPro';
-
-    if (!dbConnectionStr) {
-        console.error('DB_STRING environment variable is not set.');
-        return;
-    }
 
     try {
         const client = await MongoClient.connect(dbConnectionStr);
@@ -42,25 +21,25 @@ async function connectToDatabase() {
 }
 
 async function startApp() {
-  const db = await connectToDatabase();
+    const db = await connectToDatabase();
 
-  if (!db) {
-    console.error("Failed to connect to the database. Exiting...");
-    return;
-  }
+    if (!db) {
+        console.error('Failed to connect to the database. Exiting...');
+        return;
+    }
 
-  app.use(logger);
+    app.use(logger);
 
-  // Simple Usage (Enable All CORS Requests)
-  app.use(cors());
+    // Simple Usage (Enable All CORS Requests)
+    app.use(cors());
 
-  app.get("/", (req, res) => {
-    res.send("Hello EventProSolutions!");
-  });
+    app.get('/', (req, res) => {
+        res.send('Hello EventProSolutions!');
+    });
 
-  app.listen(env.PORT, () => {
-    console.log(`Example app listening on port ${env.PORT}`);
-  });
+    app.listen(env.PORT, () => {
+        console.log(`Example app listening on port ${env.PORT}`);
+    });
 }
 
 startApp();
