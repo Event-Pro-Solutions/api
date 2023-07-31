ed2ec2c8b24b9f8bd655776cd5821af3cc43525d
Andy <andythomas.py@gmail.com>
2023-07-31 18:23:06 -0500

    Update EventController to eventController

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import EventController from '../controllers/eventController';
+import eventController from '../controllers/eventController';
 
 const router = Router();
 
-router.get('/', EventController.getAllEvents);
-router.get('/:id', EventController.getEventById);
+router.get('/', eventController.getAllEvents);
+router.get('/:id', eventController.getEventById);
 
 export default router;
 
