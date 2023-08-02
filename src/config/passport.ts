d0767634a69bd74d6680660ebdb68f1dc2b82e38
daltkb <daltkben@gmail.com>
2023-08-02 07:15:12 -0500

    Configure sessions

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,7 +1,17 @@
 import passport from 'passport';
 import { Strategy as LocalStategy } from 'passport-local';
 import bcrypt from 'bcrypt';
-import user from '../models/user';
+import user, { IUser } from '../models/user';
+
+passport.serializeUser((user, done) => {
+    done(undefined, user);
+});
+
+passport.deserializeUser(function (user: IUser, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
 
 passport.use(
     new LocalStategy(async (username, password, done) => {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import session from 'express-session';
 import connectDB from './config/database';
 import authRoutes from './routes/auth';
 import eventRouter from './routes/events';
 
 import { env } from './config';
+import passport from 'passport';
 
 const app = express();
 const logger = morgan('short');
@@ -15,10 +17,21 @@ async function startApp() {
     await connectDB();
 
     app.use(express.json());
+    app.use(
+        session({
+            resave: false,
+            saveUninitialized: false,
+            // TODO: Update secret add it to environment variables
+            secret: 'secret',
+        })
+    );
     app.use(logger);
 
     // Simple Usage (Enable All CORS Requests)
     app.use(cors());
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     app.use('/auth', authRoutes);
     app.use('/events', eventRouter);
