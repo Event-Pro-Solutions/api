35318f30f5e6641e4f367a893340b6ad4b511bf5
Andy <andythomas.py@gmail.com>
2023-08-02 19:53:55 -0500

    Implemented middleware for checking user authentication

---
+++ src/middleware/auth.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const ensureAuth = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/");
+  }
+};
