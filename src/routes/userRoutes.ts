ed419db4f5946a8339abb1de843eb778af1bd137
Andy <andythomas.py@gmail.com>
2023-08-05 16:49:50 -0500

    Add authentication middleware to protect /:id/allEvents route **FOR TESTING**

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
+import { ensureAuth } from '../middleware/authMiddleware';
+
 
 const router = Router();
 
 router.get('/:id', userController.getAllUserEvents);
-router.get('/:id/allEvents', userController.getAllUserEvents);
+router.get('/:id/allEvents', ensureAuth, userController.getAllUserEvents);
 router.get('/:id/registeredEvents', userController.getUserRegisteredEvents);
 router.get('/:id/managedEvents', userController.getUserManagedEvents)
 
