4b46bd3adff07015f2119c22bd6421155d95afb6
Andy <andythomas.py@gmail.com>
2023-08-09 10:53:11 -0500

    Refactor server.ts for enhanced clarity and organization
    - Grouped related imports together.
    
    - Organized middleware and server configurations into distinct functions.
    
    - Added explanatory comments for each configuration section.
    
    - Reordered code logically for better maintainability.

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,24 @@ import MongoStore from 'connect-mongo';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import session from 'express-session';
+import passport from 'passport';
+
 import connectDB from './config/database';
+import { env } from './config';
 import authRouter from './routes/authRoutes';
 import eventRouter from './routes/eventRoutes';
 import userRouter from './routes/userRoutes';
 
-import { env } from './config';
-import passport from 'passport';
-
+// Initialize the express app
 const app = express();
-const logger = morgan('short');
 
-async function startApp() {
-    // Connect Database
-    await connectDB();
+// Middlewares Configuration
+function configureMiddleware() {
+    // Logger
+    const logger = morgan('short');
+    app.use(logger);
 
+    // CORS Configuration
     const corsOptions = {
         origin: 'http://localhost:3000',
         credentials: true,
@@ -27,7 +30,10 @@ async function startApp() {
     };
     app.use(cors(corsOptions));
 
+    // Express Middlewares
     app.use(express.json());
+
+    // Session Configuration
     app.use(
         session({
             resave: false,
@@ -36,26 +42,40 @@ async function startApp() {
             store: new MongoStore({ client: mongoose.connection.getClient() }),
             cookie: {
                 //TODO: Update to secure:true in deployment since we are using HTTPS on Vercel
-                secure: false,  // Assuming you're always running on HTTPS since the frontend is on Vercel.
+                secure: false,
                 httpOnly: true,
                 maxAge: 1000 * 60 * 60 * 24, // 1 day
                 sameSite: "none"
             }
         })
     );
-    
-    app.use(logger);
 
+    // Passport Configuration
     app.use(passport.initialize());
     app.use(passport.session());
+}
 
+// Routes Configuration
+function configureRoutes() {
     app.use('/auth', authRouter);
     app.use('/events', eventRouter);
     app.use('/users', userRouter);
+}
 
+// Start the server
+async function startApp() {
+    // Connect to Database
+    await connectDB();
+
+    // Configure Middlewares and Routes
+    configureMiddleware();
+    configureRoutes();
+
+    // Start the express server
     app.listen(env.PORT, () => {
-        console.log(`Example app listening on port ${env.PORT}`);
+        console.log(`Server listening on port ${env.PORT}`);
     });
 }
 
+// Trigger the server start process
 startApp();
